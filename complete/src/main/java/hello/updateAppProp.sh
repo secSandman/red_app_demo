325fa54c371eee124fca22fd797e7cfe09e2b3ca
secsandman <secsandman@gmail.com>
2018-03-05 15:18:42 -0600

    updated shell script with ec2 hitbub clone PATH

--- complete/src/main/java/hello/updateAppProp.sh
+++ complete/src/main/java/hello/updateAppProp.sh
@@ -1,6 +1,6 @@
 export key="server.address="
 export IP=$key$(ifconfig eth0 | awk '/inet / {print $2}' | cut -c 7-) ;
-export destdir=/home/ec2-user/tmp/template.properties
+export destdir=/home/ec2-user/tmp/red_app_demo/complete/src/main/java/hello
 export port="server.port=80"
 
 if [ -f "$destdir" ]
