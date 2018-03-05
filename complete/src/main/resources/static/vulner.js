4298ed3b5fdc0fbed1b600cee68c834415bbe429
secsandman <secsandman@gmail.com>
2018-03-05 14:41:22 -0600

    First git commit for red app demo. Includes spring boot, basic MVC, Bootstrap and Gradle.

---
+++ complete/src/main/resources/static/vulner.js
@@ -0,0 +1,12 @@
+if(unexpectedCondition) {
+  alert("Unexpected Condition");
+}
+
+console.log(password_entered); // Noncompliant
+
+var obj =  new Function("return " + data)(); 
+
+var myWindow = document.getElementById('myIFrame').contentWindow;
+myWindow.postMessage(message, "*"); 
+
+
