c37d0bb8b025158efeda1306e6d9ac12d6dc8ba4
secsandman <secsandman@gmail.com>
2018-03-05 18:12:32 -0600

    updated .sh file to fit the AWS OS file

--- complete/src/main/java/hello/updateAppProp.sh
+++ complete/src/main/java/hello/updateAppProp.sh
@@ -1,6 +1,7 @@
+touch application.properties
 export key="server.address="
 export IP=$key$(ifconfig eth0 | awk '/inet / {print $2}' | cut -c 7-) ;
-export destdir=/home/ec2-user/tmp/red_app_demo/complete/src/main/java/hello
+export destdir=/home/red_app_demo/complete/src/main/java/hello/application.properties
 export port="server.port=80"
 
 if [ -f "$destdir" ]
