8ea306998b5a01d52300f99e70ef1ce0fe894f80
secsandman <secsandman@gmail.com>
2018-03-07 09:57:01 -0600

    getting closer, now working on aws ec2 manually

--- complete/src/main/java/hello/updateAppProp.sh
+++ complete/src/main/java/hello/updateAppProp.sh
@@ -1,6 +1,6 @@
 touch /home/ec2-user/red_app_demo/complete/src/main/java/hello/application.properties
 export key="server.address="
-export IP=$key$(ifconfig eth0 | awk '/inet / {print $2}' | cut -c 7-) ;
+export IP=$key$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 export destdir=/home/ec2-user/red_app_demo/complete/src/main/java/hello/application.properties
 export port="server.port=80"
 if [ -f "$destdir" ]
