0fb827242704921199d704e8e61a418868272e9a
secsandman <secsandman@gmail.com>
2018-03-07 09:16:53 -0600

    updated a bunch of stuff from the original directory not my test... dummy

--- complete/src/main/java/hello/updateAppProp.sh
+++ complete/src/main/java/hello/updateAppProp.sh
@@ -1,11 +1,10 @@
-touch application.properties
+touch /home/ec2-user/red_app_demo/complete/src/main/java/hello/application.prop$
 export key="server.address="
 export IP=$key$(ifconfig eth0 | awk '/inet / {print $2}' | cut -c 7-) ;
-export destdir=/home/red_app_demo/complete/src/main/java/hello/application.properties
+export destdir=/home/ec2-user/red_app_demo/complete/src/main/java/hello/applica$
 export port="server.port=80"
-
 if [ -f "$destdir" ]
-then 
+then
     echo "$IP" >> "$destdir"
     echo "$port" >> "$destdir"
 fi
