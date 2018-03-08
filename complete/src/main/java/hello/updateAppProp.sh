2683e42f2a48c64afb4cc4058476e22194432238
secsandman <secsandman@gmail.com>
2018-03-08 08:29:24 -0600

    change the app.properties file to resources folder to test if gradle build picks it up and changes port

--- complete/src/main/java/hello/updateAppProp.sh
+++ complete/src/main/java/hello/updateAppProp.sh
@@ -1,7 +1,7 @@
-touch red_app_demo/complete/src/application.properties
+touch /home/ec2-user/red_app_demo/complete/src/main/resources/application.properties
 export key="server.address="
 export IP=$key$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
-export destdir=red_app_demo/complete/src/application.properties
+export destdir=/home/ec2-user/red_app_demo/complete/src/main/resources/application.properties
 export port="server.port=99"
 if [ -f "$destdir" ]
 then
