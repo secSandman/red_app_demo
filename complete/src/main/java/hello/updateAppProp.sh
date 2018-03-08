0479b4fe986846abf391ceec1dfa0175d973207b
secsandman <secsandman@gmail.com>
2018-03-08 08:33:18 -0600

    changed port to 80

--- complete/src/main/java/hello/updateAppProp.sh
+++ complete/src/main/java/hello/updateAppProp.sh
@@ -2,7 +2,7 @@ touch /home/ec2-user/red_app_demo/complete/src/main/resources/application.proper
 export key="server.address="
 export IP=$key$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 export destdir=/home/ec2-user/red_app_demo/complete/src/main/resources/application.properties
-export port="server.port=99"
+export port="server.port=80"
 if [ -f "$destdir" ]
 then
     echo "$IP" >> "$destdir"
