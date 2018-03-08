76b0c408128da4610ef73e300d8a2a20f2af0e5e
secsandman <secsandman@gmail.com>
2018-03-08 08:17:37 -0600

    updted the .sh to cp the application.properties in the src folder to test the tomcat servlet jar build using app.prop file in src

--- complete/src/main/java/hello/updateAppProp.sh
+++ complete/src/main/java/hello/updateAppProp.sh
@@ -1,8 +1,8 @@
-touch /home/ec2-user/red_app_demo/complete/src/main/java/hello/application.properties
+touch red_app_demo/complete/src/application.properties
 export key="server.address="
 export IP=$key$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
-export destdir=/home/ec2-user/red_app_demo/complete/src/main/java/hello/application.properties
-export port="server.port=80"
+export destdir=red_app_demo/complete/src/application.properties
+export port="server.port=99"
 if [ -f "$destdir" ]
 then
     echo "$IP" >> "$destdir"
