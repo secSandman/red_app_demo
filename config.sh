f9f5f47829667d8640b67d0f90911c883da43ff1
secsandman <secsandman@gmail.com>
2018-03-08 09:27:33 -0600

    leaving default spring boot 8080 and ommiting app.prop file for now in the .sh scripts

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 sudo yum update -y && install -y
-sudo yum remove jre-1.7.0
+sudo yum remove jre-1.7.0 -y
 sudo yum install java-1.8.0-openjdk-devel -y
 gradle_version=4.6
 sudo mkdir /opt/gradle
@@ -9,8 +9,11 @@ sudo echo "export PATH=$PATH:/opt/gradle/gradle-4.6/bin" >> .bashrc
 source ~/.bashrc
 sudo yum install git -y
 git clone https://github.com/secSandman/red_app_demo.git
-sudo chmod +x /home/ec2-user/red_app_demo/complete/src/main/java/hello/updateApp.sh
-sudo sh /home/ec2-user/red_app_demo/complete/src/main/java/hello/updateAppProp.sh
-cd red_app_demo/complete/
+cd /home/ec2-user/red_app_demo/complete/
 gradle build
-java -jar build/libs/red_app-0.1.0.jar
+cp build/libs/red_app-0.1.0.jar /home/ec2-user/
+sudo yum remove git -y
+cd /home/ec2-user/
+sudo rm -rf /opt/gradle
+sudo rm -rf /home/ec2-user/red_app_demo
+java -jar /home/ec2-user/red_app-0.1.0.jar
